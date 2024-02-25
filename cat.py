17ffc26471f9724e7a38f15d4d8804f4625a658e
Admin <admin@Admins-MacBook-Pro-8.local>
2024-02-25 17:52:55 +0530

    temp file

---
+++ cat.py
@@ -0,0 +1,13 @@
+class Cat:
+     def eat(self):
+         print("I can Eat")
+
+     def sleep(self):
+         print("I can Sleep")
+
+     def sound(self):
+         print("I can Sound")
+
+     def run(self):
+         print("I can Run")
+
