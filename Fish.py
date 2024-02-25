ca7caba639545a2a605248eb86d4a85842bba9a6
Admin <admin@Admins-MacBook-Pro-8.local>
2024-02-25 16:43:57 +0530

    fish is created

---
+++ Fish.py
@@ -0,0 +1,9 @@
+class Fish:
+    def eat(self):
+        print("I can eat")
+    def sleep(self):
+        print("I can sleep")
+    def sound(self):
+        print("I can sound")
+    def swim(self):
+        print("I can swim")
